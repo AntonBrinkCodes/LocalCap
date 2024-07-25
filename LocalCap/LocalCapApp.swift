a73ad23e57b04a6b117d33dbd62f78c1c1caa789
Anton Brink <77839398+AntonBrinkCodes@users.noreply.github.com>
2024-07-25 10:13:40 +0200

    Initial Commit

---
+++ LocalCap/LocalCapApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalCapApp.swift
+//  LocalCap
+//
+//  Created by MoveAbility Lab on 2024-07-25.
+//
+
+import SwiftUI
+
+@main
+struct LocalCapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
